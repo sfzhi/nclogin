df135891ee2f3534cbccdad57d7d6b5b22fb31e1
Sergei Zhirikov <sfzhi@yahoo.com>
2025-06-04 13:34:52 +0200

    Better detection of controlling terminal name

--- ctty.c
+++ ctty.c
@@ -17,11 +17,19 @@
 #include <stdlib.h> // strtoll()
 #include <unistd.h>
 #include <errno.h>
+/*----------------------------------------------------------------------------*/
+#define DEV_PTS_MIN 136
+#define DEV_PTS_MAX 143
+/*----------------------------------------------------------------------------*/
+#define INT_STR_BUF(t) (((sizeof(t) * 5) + 3) / 2)
 /*============================================================================*/
 static int cttyfd = -1;
 static dev_t cttydev = 0;
 static char *cttypath = NULL;
 static char *cttyname = NULL;
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+#define CTTY_NAME \
+  (((cttyname != NULL) && (*cttyname != '\0'))? cttyname: cttypath)
 /*============================================================================*/
 static dev_t get_ctty_dev(void)
 {
@@ -67,14 +75,14 @@ static bool set_ctty_path(char *path, bool standard)
   return cttypath != NULL;
 }
 /*----------------------------------------------------------------------------*/
-static bool try_tty_path(const char *path)
+static bool try_dev_path(const char *path)
 {
   struct stat st;
   return (stat(path, &st) >= 0) &&
-    S_ISCHR(st.st_mode) && (st.st_rdev == cttydev);
+    S_ISCHR(st.st_mode) && ((cttydev == 0) || (st.st_rdev == cttydev));
 }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-static char *try_tty_fd(int fd)
+static char *try_open_fd(int fd)
 {
   if (tcgetsid(fd) == getsid(0))
   {
@@ -82,7 +90,7 @@ static char *try_tty_fd(int fd)
     struct stat st;
     if ((fstat(fd, &st) >= 0) && S_ISCHR(st.st_mode) &&
         (st.st_rdev == cttydev) && ((path = ttyname(fd)) != NULL) &&
-        (strcmp(path, "/dev/tty") != 0) && try_tty_path(path))
+        (strcmp(path, "/dev/tty") != 0) && try_dev_path(path))
     {
       cttyfd = fd;
       return path;
@@ -93,19 +101,46 @@ static char *try_tty_fd(int fd)
   return NULL;
 }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-static bool try_tty_stdio(void)
+static bool try_cmd_arg(void)
 {
-  char *path = try_tty_fd(STDIN_FILENO);
+  if ((nclogin_config.ctty != NULL) && (*nclogin_config.ctty != '\0') &&
+      (strcmp(nclogin_config.ctty, "tty") != 0))
+  {
+    size_t len = strlen(nclogin_config.ctty) + 1;
+    char sbuf[strlen("/dev/") + len];
+    memcpy(sbuf + strlen("/dev/"), nclogin_config.ctty, len);
+    memcpy(sbuf, "/dev/", strlen("/dev/"));
+    return try_dev_path(sbuf) && set_ctty_path(sbuf, false);
+  }
+  return false;
+}
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+static bool try_pts_path(void)
+{
+  if ((unsigned int)major(cttydev) == DEV_PTS_MIN)
+  {
+    char sbuf[strlen("/dev/pts/") + INT_STR_BUF(unsigned int)];
+    int len = snprintf(sbuf, sizeof(sbuf),
+      "/dev/pts/%u", (unsigned int)minor(cttydev));
+    return (len > 0) && ((unsigned int)len < sizeof(sbuf)) &&
+      try_dev_path(sbuf) && set_ctty_path(sbuf, true);
+  }
+  return false;
+}
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+static bool try_stdio_fds(void)
+{
+  char *path = try_open_fd(STDIN_FILENO);
   if (path == NULL)
-    path = try_tty_fd(STDOUT_FILENO);
+    path = try_open_fd(STDOUT_FILENO);
   if (path == NULL)
-    path = try_tty_fd(STDERR_FILENO);
+    path = try_open_fd(STDERR_FILENO);
   return (path != NULL) && set_ctty_path(path, false);
 }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-static bool try_tty_links(void)
+static bool try_dev_links(void)
 {
-  char sbuf[64];
+  char sbuf[strlen("/sys/dev/char/") + 2 * INT_STR_BUF(unsigned int)];
   int len = snprintf(sbuf, sizeof(sbuf), "/sys/dev/char/%u:%u",
     (unsigned int)major(cttydev), (unsigned int)minor(cttydev));
   if ((len > 0) && ((unsigned int)len < sizeof(sbuf)))
@@ -139,66 +174,61 @@ static bool try_tty_links(void)
         standard_path = true;
       }
     }
-    if ((devpath != NULL) && try_tty_path(devpath))
-    {
-      if (cttyfd < 0)
-        cttyfd = open(devpath, O_RDONLY|O_NOATIME|O_CLOEXEC);
-      return set_ctty_path(devpath, standard_path);
-    }
+    return (devpath != NULL) &&
+      try_dev_path(devpath) && set_ctty_path(devpath, standard_path);
   }
   return false;
 }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-static bool try_open_ctty(void)
+static bool try_find_ctty(void)
 {
-  if ((nclogin_config.ctty == NULL) || (*nclogin_config.ctty == '\0') ||
-      (strcmp(nclogin_config.ctty, "tty") == 0))
+  return ((cttydev = get_ctty_dev()) != 0) && \
+    (try_cmd_arg() || try_pts_path() || try_stdio_fds() || try_dev_links());
+}
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+static bool open_new_ctty(void)
+{
+  errno = 0;
+  if (try_cmd_arg())
   {
-    failure("Unable to change controlling TTY to '%s'\n", nclogin_config.ctty);
-  }
-  else
-  {
-    size_t len = strlen(nclogin_config.ctty) + 1;
-    char sbuf[strlen("/dev/") + len];
-    memcpy(sbuf + strlen("/dev/"), nclogin_config.ctty, len);
-    memcpy(sbuf, "/dev/", strlen("/dev/"));
-    if (set_ctty_path(sbuf, true))
+    int fd = open(cttypath, O_RDWR|O_NOATIME|O_CLOEXEC);
+    if (fd >= 0)
     {
-      int fd = open(cttypath, O_RDWR|O_NOATIME|O_CLOEXEC);
-      if (fd >= 0)
+      int flags;
+      if (((fd >= 3) || (((flags = fcntl(fd, F_GETFD)) != -1) &&
+            (fcntl(fd, F_SETFD, flags&~FD_CLOEXEC) >= 0))) &&
+          ((fd == STDIN_FILENO) || (dup2(fd, STDIN_FILENO) >= 0)) &&
+          ((fd == STDOUT_FILENO) || (dup2(fd, STDOUT_FILENO) >= 0)) &&
+          ((fd == STDERR_FILENO) || (dup2(fd, STDERR_FILENO) >= 0)) &&
+          ((getsid(0) == getpid()) || (setsid() > 0)) &&
+          (ioctl(fd, TIOCSCTTY, 1) >= 0))
       {
-        int flags;
-        if (((fd >= 3) || (((flags = fcntl(fd, F_GETFD)) != -1) &&
-              (fcntl(fd, F_SETFD, flags&~FD_CLOEXEC) >= 0))) &&
-            ((fd == STDIN_FILENO) || (dup2(fd, STDIN_FILENO) >= 0)) &&
-            ((fd == STDOUT_FILENO) || (dup2(fd, STDOUT_FILENO) >= 0)) &&
-            ((fd == STDERR_FILENO) || (dup2(fd, STDERR_FILENO) >= 0)) &&
-            ((getsid(0) == getpid()) || (setsid() > 0)) &&
-            (ioctl(fd, TIOCSCTTY, 1) >= 0))
-        {
-          cttyfd = fd;
-          infomsg("Changed controlling TTY to '%s'\n", nclogin_config.ctty);
-          return true;
-        }
-        else
-        {
-          failure("Failed to switch controlling TTY: %m'\n");
-          close(fd);
-          return false;
-        }
+        cttyfd = fd;
+        infomsg("Changed controlling TTY to '%s'\n", CTTY_NAME);
+        return true;
+      }
+      else
+      {
+        failure("Failed to switch controlling TTY: %m'\n");
+        close(fd);
+        return false;
       }
     }
-    failure("Failed to open controlling TTY: %m'\n");
   }
+  else if (errno != ENOMEM)
+  {
+    errno = EINVAL;
+  }
+  failure("Failed to open controlling TTY: %m'\n");
   return false;
 }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 bool nclogin_ctty_init(void)
 {
   if (nclogin_config.changectty)
-    return try_open_ctty();
-  if (((cttydev = get_ctty_dev()) != 0) && (try_tty_stdio() || try_tty_links()))
-    infomsg("Identified controlling TTY device: %s (%s)\n", cttyname, cttypath);
+    return open_new_ctty();
+  if (try_find_ctty())
+    infomsg("Identified controlling TTY device: %s\n", CTTY_NAME);
   else
     warning("Unable to identify controlling TTY device\n");
   if (cttyfd < 0)
